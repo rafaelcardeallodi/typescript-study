382f5637c4a6291de52762489fa18d7c9e928c3a
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2021-11-24 22:30:25 -0300

    Definindo os parâmetros obrigatórios

--- src/CreateCourseService.ts
+++ src/CreateCourseService.ts
@@ -1,13 +1,13 @@
 
 interface Course{
   name: string;
-  duration: number;
+  duration?: number;
   educator: string;
 }
 
 class CreateCourseService {
 
-  execute({ name, duration, educator }: Course){
+  execute({ name, duration = 8, educator }: Course){
     console.log(name, duration, educator)
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,11 @@ import { Request, Response } from 'express';
 import CreateCourseService from './CreateCourseService';
 
 export function createCourse(req: Request, res: Response){
+  
   CreateCourseService.execute({
     name: 'NodeJS',
-    duration: 10,
     educator: 'Dani'
-  })
+  });
 
   return res.send();
 }
